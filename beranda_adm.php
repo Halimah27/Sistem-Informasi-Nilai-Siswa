79296bd63fbb1183ef448e3d0e885788b40eecfe
Halimah27 <78065592+Halimah27@users.noreply.github.com>
2021-01-27 11:38:28 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-warning">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><img src="img/Tut-Wuri-Handayani.png" width="2%"> Halaman Administrator Sistem Informasi Nilai Siswa SD XYZ</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/Tut-Wuri-Handayani.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>SISTEM INFORMASI NILAI SISWA SD XYZ</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Data Siswa.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data siswa, pengelolaan nilai siswa, <br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data siswa dan data nilai siswa.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
